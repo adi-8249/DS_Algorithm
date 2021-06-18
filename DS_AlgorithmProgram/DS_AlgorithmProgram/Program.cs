3b3931968d9e3c8fc091c262e153a1a7c66c8f70
Anjaneya <anjaneyaprasad9437@gmail.com>
2021-06-18 19:53:56 +0530

    [Anjaneya]Added: Checking for String function are equal

---
+++ DS_AlgorithmProgram/DS_AlgorithmProgram/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DS_AlgorithmProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to DS-Algorithm program");
+            String[] s1 = { "A", "B", "C" };
+            String[] s2 = { "A", "B", "C" };
+            if(TwoArrayFunction.CheckEquality(s1, s2))
+            {
+                Console.WriteLine("Both Arrays are equal");
+            }
+            else
+            {
+                Console.WriteLine("Both Array are not equal");
+            }
+        }
+    }
+}
---
+++ DS_AlgorithmProgram/DS_AlgorithmProgram/TwoArrayFunction.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DS_AlgorithmProgram
+{
+    public class TwoArrayFunction
+    {
+        public static bool CheckEquality(String[] s1, String[] s2)
+        {
+            if (s1 == s2)
+            {
+                return true;
+            }
+
+            if (s1 == null || s2 == null)
+            {
+                return false;
+            }
+
+            int n = s1.Length;
+            if (n != s2.Length)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                if (!s1[i].Equals(s2[i]))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
